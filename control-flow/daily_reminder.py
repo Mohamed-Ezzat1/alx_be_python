67fff107a932167a7ee6cf6dfa1b79a5f4159768
Mohamed-Ezzat1 <mohamedezzatshawky1@gmail.com>
2025-12-12 23:44:55 +0200

    reminder added

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,17 +4,17 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority :
     case "high" :
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             print(f"Reminder: Your high-priority task '{task}' is time-bound. Please address it immediately!")
         else:
             print(f"Reminder: Your high-priority task '{task}' needs your attention soon.")
     case "medium" :
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             print(f"Reminder: Your medium-priority task '{task}' is time-bound. Please plan accordingly.") 
         else:
             print(f"Reminder: Your medium-priority task '{task}' should be completed in due time.")
     case "low" :
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             print(f"Reminder: Your low-priority task '{task}' is time-bound. Try to fit it into your schedule.")
         else:
             print(f"Reminder: Your low-priority task '{task}' can be addressed at your convenience.")
