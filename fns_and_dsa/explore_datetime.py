5f8675b6510c1290e2d029419d942adfe2b5cd09
Mohamed-Ezzat1 <mohamedezzatshawky1@gmail.com>
2026-01-02 01:30:26 +0200

    time calculator

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,22 @@
+from datetime import datetime , timedelta
+def display_current_datetime():
+    current_date  = datetime.now()
+    formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Date and Time: {formatted_datetime}")
+    
+def calculate_future_date(days_ahead):
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days_ahead)
+    formatted_future_date = future_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Date after {days_ahead} days: {formatted_future_date}")
+    
+def main():
+    display_current_datetime()
+    days = int(input("Enter number of days to calculate future date: "))
+    calculate_future_date(days)
+    
+    
+if __name__ == "__main__":
+    main()
+    
+    
